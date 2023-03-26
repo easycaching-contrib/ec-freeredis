0cb8e62048846efb44a82e166f784e5c1b8119f1
Memoyu <mmy6076@outlook.com>
2023-03-27 00:46:50 +0800

    feat: impl provider keys

--- src/EasyCaching.FreeRedis/DefaultFreeRedisCachingProvider.Keys.cs
+++ src/EasyCaching.FreeRedis/DefaultFreeRedisCachingProvider.Keys.cs
@@ -11,72 +11,124 @@
 
         public bool KeyDel(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return _cache.Del(cacheKey) == 0;
         }
 
         public async Task<bool> KeyDelAsync(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return await _cache.DelAsync(cacheKey) == 0;
         }
 
         public bool KeyExpire(string cacheKey, int second)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return _cache.Expire(cacheKey, second);
         }
 
         public async Task<bool> KeyExpireAsync(string cacheKey, int second)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return await _cache.ExpireAsync(cacheKey, second);
         }
 
         public bool KeyPersist(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return _cache.Persist(cacheKey);
         }
 
         public async Task<bool> KeyPersistAsync(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return await _cache.PersistAsync(cacheKey);
         }
 
         public bool KeyExists(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return _cache.Exists(cacheKey);
         }
 
         public async Task<bool> KeyExistsAsync(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return await _cache.ExistsAsync(cacheKey);
         }
 
         public long TTL(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return _cache.Ttl(cacheKey);
         }
 
         public async Task<long> TTLAsync(string cacheKey)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            return await _cache.TtlAsync(cacheKey);
         }
 
         public object Eval(string script, string cacheKey, List<object> args)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(script, nameof(script));
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            var res = _cache.Eval(script, new[] { cacheKey }, args.ToArray());
+            return res;
         }
 
         public async Task<object> EvalAsync(string script, string cacheKey, List<object> args)
         {
-            throw new NotImplementedException();
+            ArgumentCheck.NotNullOrWhiteSpace(script, nameof(script));
+            ArgumentCheck.NotNullOrWhiteSpace(cacheKey, nameof(cacheKey));
+
+            var res = _cache.EvalAsync(script, new[] { cacheKey }, args.ToArray());
+            return res;
         }
 
         public List<string> SearchKeys(string cacheKey, int? count)
         {
-            throw new NotImplementedException();
+            var keys = new List<string>();
+
+            long nextCursor = 0;
+            do
+            {
+                var scanResult = _cache.Scan(nextCursor, cacheKey, count ?? 250, string.Empty);
+                nextCursor = scanResult.cursor;
+                var items = scanResult.items;
+                keys.AddRange(items);
+            }
+            while (nextCursor != 0);
+
+            return keys;
         }
 
         public async Task<List<string>> SearchKeysAsync(string cacheKey, int? count)
         {
-            throw new NotImplementedException();
+            var keys = new List<string>();
+
+            long nextCursor = 0;
+            do
+            {
+                var scanResult = await _cache.ScanAsync(nextCursor, cacheKey, count ?? 250, string.Empty);
+                nextCursor = scanResult.cursor;
+                var items = scanResult.items;
+                keys.AddRange(items);
+            }
+            while (nextCursor != 0);
+
+            return keys;
         }
     }
 }
